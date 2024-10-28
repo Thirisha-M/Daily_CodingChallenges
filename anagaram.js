77a538f996b4461f6c5a52e177e724d5469daf30
Thirisha-M <thirisha.m20@gmail.com>
2024-10-28 11:43:30 +0530

    updated file

--- anagaram.js
+++ anagaram.js
@@ -1,34 +1,38 @@
-function anagaram(as:string,bs:string):any{
-    let val1=as.toLowerCase();
-    let val2=bs.toLowerCase();
-    val1=as.split('').sort().join('');
-    val2=bs.split('').sort().join('');
-    if(val1==val2)
-    {
+function anagaram(as, bs) {
+    var val1 = as.toLowerCase();
+    var val2 = bs.toLowerCase();
+    val1 = as.split('').sort().join('');
+    val2 = bs.split('').sort().join('');
+    if (val1 == val2) {
         return "anagram";
     }
-    else{
-    return  "Not";
+    else {
+        return "Not";
     }
 }
-console.log(anagaram("Dad","dad"))
-var Input = require('prompt-sync')();
-var age1 = Input("Enter age ");
-var Age = Number(age1);
-var sex = Input("Enter the Sex ");
-var materialStatus = Input("Enter the MaterialStatus ");
-function employeeDetails(Age, sex, materialStatus) {
-    if (sex == "F") {
-        return "She will work only in urban areas";
-    }
-    else if (sex == "M") {
-        if (Age >= 20 && Age <= 40) {
-            return "he may work in anywhere";
-        }
-        else if (Age >= 40 && Age <= 60) {
-            return "he will work in urban areas only";
-        }
-    }
-    return "ERROR";
-}
-console.log(employeeDetails(Age, sex, materialStatus));
+console.log(anagaram("listen", "Dad"));
+// const Input=require('prompt-sync')();
+// const age1=Input("Enter age ");
+// const Age=Number(age1);
+// const sex=Input("Enter the Sex ");
+// const materialStatus=Input("Enter the MaterialStatus ");
+// function employeeDetails(Age:number,sex:string,materialStatus:string):string
+// {
+//     if(sex=="F")
+//     {
+//         return "She will work only in urban areas";
+//     }
+//     else if(sex=="M") 
+//     {
+//         if(Age>=20 && Age<=40)
+//         {
+//             return "he may work in anywhere";
+//         }
+//         else if(Age>=40 && Age<=60)
+//         {
+//             return "he will work in urban areas only";
+//         }  
+//     }
+//         return "ERROR";
+// }
+// console.log(employeeDetails(Age,sex,materialStatus));
