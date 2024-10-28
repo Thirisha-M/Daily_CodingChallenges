100e50d8e6035488f1563f92eaeefb17b3131b72
Thirisha-M <thirisha.m20@gmail.com>
2024-10-28 10:48:35 +0530

    updated file

--- anagaram.js
+++ anagaram.js
@@ -1,22 +1,34 @@
-// const users=require('prompt-sync')();
-// var as:string=users ("Enter firststring");
-// var bs:string=users("Enter secondstring");
-// function anagaram(as,bs):any{
-//     let val1=as.toLowerCase();
-//     let val2=bs.toLowerCase();
-//     val1=as.split('').sort().join('');
-//     val2=bs.split('').sort().join('');
-//     if(val1==val2)
-//     {
-//         return "anagram";
-//     }
-//     else{
-//     return  "Not";
-//     }
-// }
-// console.log(anagaram(as,bs));
-function duplica(arr) {
-    var num = [new Set(arr)]; // Convert Set back to an array using spread operator
-    return num;
+function anagaram(as:string,bs:string):any{
+    let val1=as.toLowerCase();
+    let val2=bs.toLowerCase();
+    val1=as.split('').sort().join('');
+    val2=bs.split('').sort().join('');
+    if(val1==val2)
+    {
+        return "anagram";
+    }
+    else{
+    return  "Not";
+    }
 }
-console.log(duplica([1, 2, 1]));
+console.log(anagaram("Dad","dad"))
+var Input = require('prompt-sync')();
+var age1 = Input("Enter age ");
+var Age = Number(age1);
+var sex = Input("Enter the Sex ");
+var materialStatus = Input("Enter the MaterialStatus ");
+function employeeDetails(Age, sex, materialStatus) {
+    if (sex == "F") {
+        return "She will work only in urban areas";
+    }
+    else if (sex == "M") {
+        if (Age >= 20 && Age <= 40) {
+            return "he may work in anywhere";
+        }
+        else if (Age >= 40 && Age <= 60) {
+            return "he will work in urban areas only";
+        }
+    }
+    return "ERROR";
+}
+console.log(employeeDetails(Age, sex, materialStatus));
