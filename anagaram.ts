ffd92c1a3cc2439706dcfde56adb153f00f2c97c
Thirisha-M <thirisha.m20@gmail.com>
2024-11-05 17:59:40 +0530

    updated file

--- anagaram.ts
+++ anagaram.ts
@@ -8,37 +8,14 @@ function anagaram(as:string,bs:string):any{
     {
         return "anagram";
     }
-    else{
-    return  "Not";
+    else
+    {
+        return  "Not";
     }
 
 }
 console.log(anagaram("listen","Dad"))
 
-// const Input=require('prompt-sync')();
-// const age1=Input("Enter age ");
-// const Age=Number(age1);
-// const sex=Input("Enter the Sex ");
-// const materialStatus=Input("Enter the MaterialStatus ");
 
 
-// function employeeDetails(Age:number,sex:string,materialStatus:string):string
-// {
-//     if(sex=="F")
-//     {
-//         return "She will work only in urban areas";
-//     }
-//     else if(sex=="M") 
-//     {
-//         if(Age>=20 && Age<=40)
-//         {
-//             return "he may work in anywhere";
-//         }
-//         else if(Age>=40 && Age<=60)
-//         {
-//             return "he will work in urban areas only";
-//         }  
-//     }
-//         return "ERROR";
-// }
-// console.log(employeeDetails(Age,sex,materialStatus));
+
